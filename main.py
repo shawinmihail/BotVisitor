d07e483c9f21fe0aeae042880394f51f1a1a8965
shawinmihail <shawinmihail@gmail.com>
2015-10-31 20:35:12 +0300

    first trying

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-# -*- coding: utf-8 -*-
+# encoding: utf-8
+from PyQt5 import Qt, QtCore, QtWidgets, QtWebKit, QtWebKitWidgets, QtWebChannel, QtWebSockets, QtNetwork
+import sys
+from time import sleep
 
+
+class Browser(QtWebKitWidgets.QWebView):
+
+    def __init__(self):
+        QtWebKitWidgets.QWebView.__init__(self)
+
+        url = Qt.QUrl("http://vk.com/im?sel=265355677")
+        url = Qt.QUrl("http://rbk.ru/")
+        url = Qt.QUrl("http://google.com")
+        self.load(url)
+        page = self.page()
+        element = page.mainFrame().findFirstElement("input[name=sfdiv]")
+        element.setAttribute("value", "value")
+
+
+app = QtWidgets.QApplication(sys.argv)
+main_pane = Browser()
+main_pane.show()
+sys.exit(app.exec_())
